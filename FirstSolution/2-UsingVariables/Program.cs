ca2ca2880d382e24b9416b4dc428e10807795537
Bruno Habermann <bruno.habermann@dextra-sw.com>
2020-10-25 23:02:41 -0300

    Adiciona segundo projeto

---
+++ FirstSolution/2-UsingVariables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_UsingVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executing Second Project - Using Variables");
+
+
+
+            Console.WriteLine("The execution ends here. Press Enter to Exit...");
+            Console.ReadLine();
+        }
+    }
+}
